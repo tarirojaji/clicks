d972032cfb8782022078435dccb7a2950600a57b
jaji <57304962+tarirojaji@users.noreply.github.com>
2021-09-04 13:33:22 +0100

    init

---
+++ cheat.js
@@ -0,0 +1,137 @@
+// const startButton = document.querySelector('[title="Start Module Button"]')
+
+
+// function cheat() {
+// 	const nextButton = document.getElementById('nextButton')
+// 	if (typeof(click1()) != undefined) {
+// 		click1()
+// 	} else {
+// 		console.log('pass')
+// 	}
+
+// 	StartClick()
+
+// 	if (nextButton.classList == 'squirrel') {
+// 	NavNextBtnClk()
+// 	} else {
+// 		// pass
+// 	}
+
+// 	setTimeout(cheat, 5000);
+// }
+
+// cheat();
+
+
+function click1() {
+	console.log('click1');
+}
+function click2() {
+	console.log('click2');
+}
+function click3() {
+	console.log('click3');
+}
+
+
+
+function cheat() {
+
+	for (let i=1; i < 10; i++) {
+		console.log(`${click}${1}1`);
+		// if (typeof `${click}`+`${i}` == 'function') {
+		// click1();
+		// }
+	}
+
+	// NavNextBtnClk()
+	// setTimeout(cheat, 5000);
+}
+
+cheat();
+
+
+
+
+function cheat() {
+
+	if (typeof click1 == 'function') {
+		click1();
+		click2();
+		click3();
+		click4();
+	}
+
+	NavNextBtnClk()
+	setTimeout(cheat, 5000);
+}
+
+cheat();
+
+
+function cheat2() {
+
+	for (let i = 1; i<=15; i++) {
+		if (typeof clicks == 'function') {
+			clicks(i);
+		} else if ((typeof iconClick == 'function')) { 
+			iconClick(i)
+		} else if ((typeof clickImg == 'function')) { 
+			clickImg(i)
+		}
+		
+	}
+	NavNextBtnClk()
+	setTimeout(cheat2, 5000);
+}
+cheat2();
+
+while (typeof clickImg == 'function') {
+	for (let i = 1; i<15; i++) {
+		clickImg(i)
+	}
+}
+
+clickImg(1)
+clickImg(2)
+clickImg(3)
+clickImg(4)
+clickImg(5)
+clickImg(6)
+clickImg(7)
+clickImg(8)
+clickImg(9)
+clickImg(10)
+clickImg(1)
+clickImg(1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function cheat() {
+	NavNextBtnClk()
+	setTimeout(cheat, 5000);
+}
+
+cheat();
